634e347fd198e9ba4290fddf7d1d29281e32b0a1
kunal.kulthe <kunal.kulthe@ril.com>
2025-02-16 15:19:06 +0530

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    basePath: '/portfolio', // Your GitHub repository name
+    assetPrefix: '/portfolio/', // Your GitHub repository name
+    output: 'export', // Static export configuration
+};
 
 export default nextConfig;
